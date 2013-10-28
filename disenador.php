ffa2be21c5abe7d26af723fec28449059b0948c4
hapu <hapu.x2@gmail.com>
2013-10-28 13:35:24 -0600

    Primer Modificacion a Diseñadores

--- disenador.php
+++ disenador.php
@@ -1,1 +1,24 @@
-﻿/* NO SE NOS OCURRIO NADA :( */
+﻿<?php
+/* NO SE NOS OCURRIO NADA :( */
+require_once "empleado.class.php";
+switch ($_POST['etapa'])
+{
+
+	default:
+		require_once 'etapa0.html';
+		break;
+	case '0';
+	require_once 'etapa1.html';
+	break;
+	case '1';
+	require_once 'etapa2.html';
+	break;
+	case '2';
+	require_once 'etapa3.html';
+	break;
+	case '3';
+	echo 'Fin';
+	break;
+
+}
+?>
