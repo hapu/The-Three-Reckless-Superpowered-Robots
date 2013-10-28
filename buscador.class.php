6924c8b71490ad43f85376c345eabb3269bd4d00
DavidLanderosAlcala <dh.landeros08@gmail.com>
2013-10-27 23:38:16 -0600

    Otravez yo ¬¬
    no se vio la carpeta include

---
+++ buscador.class.php
@@ -0,0 +1,24 @@
+<?php
+require_once("empleado.class.php");
+
+class Buscador
+{
+    var $last_error;
+    function __construct()
+    {
+
+    }
+
+    function buscarPorCapacidad($puesto)
+    {
+          // retorna una lista de Empleados capaces de trabajar en un puesto
+    }
+
+    function mostrarCostos($puesto, $cantidad = false)
+    {
+          // retorna un arreglo de Puestos afectados por mover
+          // a un empleado en un puesto dado
+    }
+}
+
+?>
